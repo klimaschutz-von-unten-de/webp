e9c53cc134e77cf1a85c5f01a4c5f4192d1ead31
Stefan Ruppert <stefan@klimaschutz-von-unten.de>
2022-03-19 17:48:20 +0100

    script cleanup

--- ksvu-webp.sh
+++ ksvu-webp.sh
@@ -1,28 +1,35 @@
 #!/usr/bin/env bash
-
+#
+# Author: Stefan Ruppert <webp@klimaschutz-von-unten.de>
+# License: GNU GENERAL PUBLIC LICENSE Version 3
+#
 # pre-conditions
 # installed tools: gimp, imagemagick (convert, identity), pngquant,
 #                  awk, sed and firefox for comparing converted and
 #                  original images 
 #
 
+# script options
+opt_compare=0
+opt_compare_conv=0
+opt_fixhtml=0
+opt_force=0
+opt_height=0
+opt_list=0
+opt_lossless=-1
+opt_midfix=""
+opt_overview=0
+opt_pngcolors=256
+opt_pngquant=0
+opt_quality=70
+opt_scale=0
+opt_verbose=1
+opt_width=0
+
 # script variables
-compare=0
-compare_conv=0
-verbose=1
-list=0
-width=0
-height=0
-percent=0
-quality=70
-lossless=0
-png=0
-cmd_lossless=-1
-cmd_pngquant=0
-midfix=""
-overview=0
-fixhtml=0
-force=0
+
+var_lossless=0
+var_png=0
 
 function ksvu-webp-help()
 {
@@ -33,7 +40,8 @@ function ksvu-webp-help()
     echo "-C, --compare-conv"
     echo "    compare imagemagick and gimp converted images using 2 new firefox windows"
     echo "-f, --fixhtml"
-    echo "    change any reference from .jpg, .gif, .png to .webp using sed"
+    echo "    change any reference from .jpg, .gif, .png to .webp using sed within passed"
+    echo "    html files"
     echo "--force"
     echo "    force installing gimp ksvu-scale-webp.scn script"
     echo "--help"
@@ -51,9 +59,11 @@ function ksvu-webp-help()
     echo "    prints overview of old (jpg, png, gif) and new webp image file size"
     echo "-q[percent], --quality[=percent]"
     echo "    specifies the quality factor used lossy compression mode,"
-    echo "    disables lossless encoding"
+    echo "    disables lossless encoding. Default is 70."
     echo "-Q, --pngquant"
     echo "    for png images run pngquant tool"
+    echo "--pngcolors[=colors]"
+    echo "    number of colors to use for pngquant"
     echo "--quiet"
     echo "    sets verbose mode to zero avoiding any output"
     echo "-s[percent], --scale[=percent]"
@@ -69,7 +79,7 @@ function ksvu-gimp-prepare()
     version=$(gimp --version | cut -d" " -f6 | cut -d"." -f1-2)
     confdir=$HOME/.config/GIMP/$version/scripts
     if test -d $confdir; then
-	if test ! -f $confdir/ksvu-scale-webp.scm -o $force -eq 1; then
+	if test ! -f $confdir/ksvu-scale-webp.scm -o $opt_force -eq 1; then
 	    echo "Installing ksvu-scale-webp.scm into $confdir directory"
 	    cat >$confdir/ksvu-scale-webp.scm <<EOF
 (define (ksvu-scale-webp in_filename out_filename width height quality lossless)
@@ -91,21 +101,21 @@ function ksvu-printf()
 {
     level=$1
     shift
-    if test $verbose -ge $level; then
+    if test $opt_verbose -ge $level; then
 	printf "$*"
     fi
 }
 
 function ksvu-webp-convert-gimp()
 {
-    imgwidth=$width
-    imgheight=$height
+    imgwidth=$opt_width
+    imgheight=$opt_height
     # get image width and height
     inpwidth=$(identify -format "%w" "$1")
     inpheight=$(identify -format "%h" "$1")
-    if test $percent -gt 0; then
-	imgwidth=$(echo "" | awk -v iw=$inpwidth -v p=$percent 'END { printf "%.0f", (iw*p)/100 }')
-	ksvu-printf 2 " (percent=$percent: $inpwidth => $imgwidth)"
+    if test $opt_scale -gt 0; then
+	imgwidth=$(echo "" | awk -v iw=$inpwidth -v p=$opt_scale 'END { printf "%.0f", (iw*p)/100 }')
+	ksvu-printf 2 " (scale=$opt_scale%%: $inpwidth => $imgwidth)"
     fi
     if test $imgwidth -eq 0; then
 	imgwidth=$inpwidth
@@ -113,22 +123,24 @@ function ksvu-webp-convert-gimp()
     if test $imgheight -eq 0; then
 	tmppercent=$(echo "" | awk -v iw=$inpwidth -v w=$imgwidth 'END { printf "%d", w*100/iw }')
 	imgheight=$(echo "" | awk -v iw=$inpwidth -v ih=$inpheight -v w=$imgwidth 'END { printf "%d", ih*w/iw }')
-	ksvu-printf 2 " (height=0: $inpwidth => $imgwidth [$tmppercent%%], height:$imgheight)"
+	ksvu-printf 2 " (height=0: width:$inpwidth => $imgwidth [$tmppercent%%], height:$imgheight)"
+    else
+	imgwidth=$(echo "" | awk -v iw=$inpwidth -v ih=$inpheight -v h=$imgheight 'END { printf "%d", iw*h/ih }')
     fi
-    ksvu-printf 2 " ($1[$inpwidth,$inpheight,lossless=$lossless] -> $2[$imgwidth,$imgheight])"
-    gimp -i -b "(ksvu-scale-webp \"$1\" \"$2\" $imgwidth $imgheight $quality $lossless)" -b "(gimp-quit 0)" 2>/dev/null
+    ksvu-printf 2 " ($1[$inpwidth,$inpheight,lossless=$var_lossless] -> $2[$imgwidth,$imgheight])"
+    gimp -i -b "(ksvu-scale-webp \"$1\" \"$2\" $imgwidth $imgheight $opt_quality $var_lossless)" -b "(gimp-quit 0)" 2>/dev/null
 }
 
 function ksvu-webp-convert-magick()
 {
-    imgwidth=$width
-    imgheight=$height
+    imgwidth=$opt_width
+    imgheight=$opt_height
     # get image width and height
     inpwidth=$(identify -format "%w" "$1")
     inpheight=$(identify -format "%h" "$1")
-    if test $percent -gt 0; then
-	imgwidth=$(echo "" | awk -v iw=$inpwidth -v p=$percent 'END { printf "%.0f", (iw*p)/100 }')
-	ksvu-printf 2 " (percent=$percent: $inpwidth => $imgwidth)"
+    if test $opt_scale -gt 0; then
+	imgwidth=$(echo "" | awk -v iw=$inpwidth -v p=$opt_scale 'END { printf "%.0f", (iw*p)/100 }')
+	ksvu-printf 2 " (scale=$opt_scale%%: $inpwidth => $imgwidth)"
     fi
     if test $imgwidth -eq 0; then
 	imgwidth=$inpwidth
@@ -136,22 +148,24 @@ function ksvu-webp-convert-magick()
     if test $imgheight -eq 0; then
 	tmppercent=$(echo "" | awk -v iw=$inpwidth -v w=$imgwidth 'END { printf "%d", w*100/iw }')
 	imgheight=$(echo "" | awk -v iw=$inpwidth -v ih=$inpheight -v w=$imgwidth 'END { printf "%d", ih*w/iw }')
-	ksvu-printf 2 " (height=0: $inpwidth => $width [$tmppercent%%], height:$imgheight)"
+	ksvu-printf 2 " (height=0: width:$inpwidth => $opt_width [$tmppercent%%], height:$imgheight)"
+    else
+	imgwidth=$(echo "" | awk -v iw=$inpwidth -v ih=$inpheight -v h=$imgheight 'END { printf "%d", iw*h/ih }')
     fi
 
     magick_lossless=""
-    if test $lossless -eq 1; then
+    if test $var_lossless -eq 1; then
 	magick_lossless="-define webp:lossless=true"
     fi
-    if test $cmd_lossless -ne -1; then
-	if test $lossless -eq 1; then
+    if test $opt_lossless -ne -1; then
+	if test $var_lossless -eq 1; then
 	    magick_lossless="-define webp:lossless=true"
 	else
 	    magick_lossless="-define webp:lossless=false"
 	fi
     fi
-    ksvu-printf 2 " ($1[$inpwidth,$inpheight,lossless=$lossless] -> $2[$imgwidth,$imgheight])"
-    convert -quality $quality -resize "${imgwidth}x${imgheight}" $magick_lossless -define webp:thread-level=0 -define webp:pass=10 -define webp:preprocessing=1 -define webp:method=6 -define webp:image-hint:photo "$1" "$2"
+    ksvu-printf 2 " ($1[$inpwidth,$inpheight,lossless=$var_lossless] -> $2[$imgwidth,$imgheight])"
+    convert -quality $opt_quality -resize "${imgwidth}x${imgheight}" $magick_lossless -define webp:thread-level=0 -define webp:pass=10 -define webp:preprocessing=1 -define webp:method=6 -define webp:image-hint:photo "$1" "$2"
 }
 
 function ksvu-compress-ratio()
@@ -192,7 +206,7 @@ function ksvu-size-overview()
     echo "" | awk -v os=$oldsize -v ns=$newsize ' END { printf "Reduction: %.1f%%\n", (1 - ns/os)*100; }'
 }
 
-OPTS=$(getopt -o v::cCl:Ls:w:h:oq:Qfm: --long verbose::,help,compare,compare-conv,lossless:,list,scale:,width:,height:,overview,quality:,pngquant,midfix:,fixhtml,force,quiet -n "ksvu-webp" -- "$@")
+OPTS=$(getopt -o v::cCl:Ls:w:h:oq:Qfm: --long verbose::,help,compare,compare-conv,lossless:,list,scale:,width:,height:,overview,quality:,pngquant,pngcolors:,midfix:,fixhtml,force,quiet -n "ksvu-webp" -- "$@")
 
 if test $? != 0; then
     echo "Terminating..." >&2
@@ -206,10 +220,10 @@ while true ; do
 	-v|--verbose)
 	    case "$2" in
 		"")
-		    verbose=1
+		    opt_verbose=1
 		    ;;
 		*)
-		    verbose=$2
+		    opt_verbose=$2
 		    ;;
 	    esac
 	    shift 2
@@ -218,60 +232,64 @@ while true ; do
 	    ksvu-webp-help
 	    ;;
 	--quiet)
-	    verbose=0
+	    opt_verbose=0
 	    shift
 	    ;;
 	-c|--compare)
-	    compare=1
+	    opt_compare=1
 	    shift
 	    ;;
 	-C|--compare-conv)
-	    compare_conv=1
+	    opt_compare_conv=1
 	    shift
 	    ;;
 	-l|--lossless)
-	    cmd_lossless=$2
+	    opt_lossless=$2
 	    shift 2
 	    ;;
 	-L|--list)
-	    list=1
+	    opt_list=1
 	    shift
 	    ;;
 	-s|--scale)
-	    percent=$2
+	    opt_scale=$2
 	    shift 2
 	    ;;
 	-w|--width)
-	    width=$2
+	    opt_width=$2
 	    shift 2
 	    ;;
 	-h|--height)
-	    height=$2
+	    opt_height=$2
 	    shift 2
 	    ;;
 	-o|--overview)
-	    overview=1
+	    opt_overview=1
 	    shift 1
 	    ;;
 	-q|--quality)
-	    cmd_lossless=0
-	    quality=$2
+	    opt_lossless=0
+	    opt_quality=$2
 	    shift 2
 	    ;;
 	-Q|--pngquant)
-	    cmd_pngquant=1;
+	    opt_pngquant=1;
 	    shift 1
 	    ;;
+	--pngcolors)
+	    opt_pngcolors=$2
+	    shift 2
+	    ;;
 	-m|--midfix)
-	    midfix="$2"
+	    opt_midfix="$2"
 	    shift 2
 	    ;;
 	--force)
-	    force=1
+	    opt_force=1
 	    shift
 	    ;;
 	-f|--fixhtml)
-	    fixhtml=1
+	    opt_fixhtml=1
 	    shift
 	    ;;
 	--)
@@ -285,24 +303,24 @@ while true ; do
     esac
 done
 
-if test $fixhtml -eq 1; then
+if test $opt_fixhtml -eq 1; then
     for arg; do
 	echo "$arg: fixing html images to .webp"
 	sed -i -e 's/\.png/\.webp/g' -e 's/\.jpg/\.webp/g' -e 's/\.gif/\.webp/g' $arg
     done
     exit 0
-elif test $overview -eq 1; then
+elif test $opt_overview -eq 1; then
     ksvu-size-overview
     exit 0
 fi
 
 ksvu-printf 3 "Parameters used:\n"
-ksvu-printf 3 "    width: $width\n"
-ksvu-printf 3 "    height: $height\n"
-ksvu-printf 3 "    lossless: $cmd_lossless\n"
-ksvu-printf 3 "    quality: $quality\n"
-ksvu-printf 3 "    percent: $percent\n"
-ksvu-printf 3 "    midfix: $midfix\n"
+ksvu-printf 3 "    width: $opt_width\n"
+ksvu-printf 3 "    height: $opt_height\n"
+ksvu-printf 3 "    lossless: $opt_lossless\n"
+ksvu-printf 3 "    quality: $opt_quality\n"
+ksvu-printf 3 "    scale: $opt_scale\n"
+ksvu-printf 3 "    midfix: $opt_midfix\n"
 
 ksvu-gimp-prepare
 
@@ -315,33 +333,33 @@ for arg; do
     lsuffix=$(echo $suffix | tr "[A-Z]" "[a-z]")
     case $suffix in
 	png)
-	    png=1
-	    lossless=1
+	    var_png=1
+	    var_lossless=1
 	    ;;
 	gif)
-	    lossless=1
+	    var_lossless=1
 	    ;;
 	jpg|jpeg)
-	    lossless=0
+	    var_lossless=0
 	    ;;
 	*)
-	    lossless=0
+	    var_lossless=0
 	    ;;
     esac
-    if test $cmd_lossless -ne -1; then
-	lossless=$cmd_lossless
+    if test $opt_lossless -ne -1; then
+	var_lossless=$opt_lossless
     fi
     name=$(basename "$arg" .$suffix)
     inp="$arg"
     inp2="$inp"
     
     ksvu-printf 1 "Converting $inp"
-    if test $png -eq 1 -a $cmd_pngquant -eq 1; then
+    if test $var_png -eq 1 -a $opt_pngquant -eq 1; then
 	ksvu-printf 1 " -> $name-q.png "
-	pngquant --speed 1 --nofs 256 -o "$name-q.png" "$inp"
+	pngquant --speed 1 --nofs $opt_pngcolors -o "$name-q.png" "$inp"
 	inp2="$name-q.png"
     fi
-    out="${name}${midfix}"
+    out="${name}${opt_midfix}"
     outfile="${out}.webp"
     ksvu-printf 1 " -> $outfile ..."
     ksvu-webp-convert-gimp "$inp2" "$out-gimp.webp"
@@ -352,7 +370,7 @@ for arg; do
     ksvu-compress-ratio magick "$inp2" "$out-magick.webp"
     size_magick=$outsize
 
-    if test $compare_conv -eq 1; then
+    if test $opt_compare_conv -eq 1; then
 	firefox --new-window "$out-magick.webp" --new-window "$out-gimp.webp"
 	sleep 5
     fi
@@ -365,14 +383,14 @@ for arg; do
 	mv "$out-magick.webp" "$outfile"
 	rm "$out-gimp.webp"
     fi
-    if test $compare -eq 1; then
+    if test $opt_compare -eq 1; then
 	firefox --new-window "$inp" --new-window "$out.webp"
     fi
-    if test $verbose -eq 1; then
+    if test $opt_verbose -eq 1; then
 	ksvu-compress-ratio "" "$inp" "$out.webp"
     fi
     ksvu-printf 1 " done\n"
-    if test $list -eq 1; then
+    if test $opt_list -eq 1; then
 	ls -lrt "${name}"*
     fi
     if test -f "$name-q.png"; then
